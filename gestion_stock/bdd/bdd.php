3cd261fbba2e9e38b73efe3fec23398e4a154af8
youssna <nasefyoussef@outlook.com>
2025-10-30 12:13:32 +0100

    Delete gestion_stock directory

--- gestion_stock/bdd/bdd.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=gestion_stock;charset=utf8", "root", "root");
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur : " . $e->getMessage());
-}
-?>
--- gestion_stock/view/commun/footer.php
+++
@@ -1,6 +0,0 @@
-    <footer>
-        <p>&copy; <?= date('Y'); ?> ALMI Logistique - Application de gestion de stock</p>
-    </footer>
-
-</body>
-</html>
